b22b3b0608731f025fcbeb6dd9cb4637030401b7
Parth Garg <parth@dunzo.in>
2021-05-10 15:14:38 +0530

    initial setup

---
+++ GithubAvatar/GithubAvatar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GithubAvatar
+//
+//  Created by Parth Garg on 10/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
